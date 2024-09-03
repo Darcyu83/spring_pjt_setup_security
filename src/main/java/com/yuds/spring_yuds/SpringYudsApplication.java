ceeffdfa01647b0ba3bd053dfae9a29c54717354
yuds <yuds@linkconnection.co.kr>
2024-09-03 13:49:03 +0900

    Spring-security Boilerplate

---
+++ src/main/java/com/yuds/spring_yuds/SpringYudsApplication.java
@@ -0,0 +1,13 @@
+package com.yuds.spring_yuds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringYudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringYudsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuds/spring_yuds/SpringYudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuds.spring_yuds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringYudsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
